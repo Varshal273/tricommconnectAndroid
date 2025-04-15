28e408cc008c601ba6914a827cae50668d7d3419
drChaos1728 <dr.chaos1770@gmail.com>
2025-04-15 16:41:17 +0530

    Revert "Version_1_Completed"
    This reverts commit 77dac8b574f406a666c3b7a54d92e186ea114ad3.

--- app/src/main/java/com/example/tricommconnect_v1/data/model/ChatListResponse.kt
+++
@@ -1,5 +0,0 @@
-package com.example.tricommconnect_v1.data.model
-
-data class ChatListResponse(
-    val chatNames: List<String>
-)
--- app/src/main/java/com/example/tricommconnect_v1/data/model/LoginRequest.kt
+++
@@ -1,6 +0,0 @@
-package com.example.tricommconnect_v1.data.model
-
-data class LoginRequest(
-    val email: String,
-    val password: String
-)
